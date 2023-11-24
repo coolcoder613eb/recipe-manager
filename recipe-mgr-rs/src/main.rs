545c18547e132fa093fee977cccc3ebb248b0a87
coolcoder613eb <96163908+coolcoder613eb@users.noreply.github.com>
2023-11-24 12:59:20 +1100

    update (sorry)

--- recipe-mgr-rs/src/main.rs
+++ recipe-mgr-rs/src/main.rs
@@ -1,5 +1,131 @@
+use serde_json;
+use serde_json::json;
+use slint::{ModelRc, SharedString, VecModel};
+use std::fs;
+use std::rc::Rc;
+
+const FILE_PATH: &str = "recipes.json";
+
+fn load_data() -> serde_json::Value {
+    match fs::read_to_string(FILE_PATH) {
+        Ok(contents) => serde_json::from_str(&contents).unwrap(),
+        Err(_) => serde_json::Value::Object(serde_json::Map::new()),
+    }
+}
+
+fn save_data(data: &serde_json::Value) {
+    fs::write(FILE_PATH, serde_json::to_string_pretty(data).unwrap()).unwrap();
+}
+
+fn open_editor() {
+    let ew = EditWindow::new().unwrap();
+    let weak = ew.as_weak();
+    ew.on_return_from_editor(move |a, b, c| return_from_editor(&a, &b, &c, &weak.unwrap()));
+    ew.show().unwrap();
+}
+
+fn new_recipe(window: &MainWindow) {
+    
+}
+
 fn main() {
-    EditWindow::new().unwrap().run().unwrap();
+    /*
+    let ew = EditWindow::new().unwrap();
+    let weak = ew.as_weak();
+    ew.on_return_from_editor(move |a, b, c| return_from_editor(&a, &b, &c, &weak.unwrap()));
+    ew.run().unwrap();
+    */
+    let mut data = load_data();
+    let window = MainWindow::new().unwrap();
+    let recipes: Vec<SharedString> = data["recipes"]
+        .as_array()
+        .unwrap()
+        .iter()
+        .map(|recipe| SharedString::from(recipe["name"].as_str().unwrap()))
+        .collect();
+    let recipes_slice: &[SharedString] = &recipes;
+    println!("{:?}", recipes_slice);
+    window.set_recipes(VecModel::from_slice(recipes_slice));
+    let weak = window.as_weak();
+    window.on_new_recipe(move || new_recipe(&weak.unwrap()));
+    window.run().unwrap();
+}
+
+fn return_from_editor(name: &str, desc: &str, ingredients: &str, ew: &EditWindow) {
+    println!("Name: {name}, Desc: {desc}, Ingredients: {:?}", ingredients);
+    let _ = ew.hide();
+}
+
+slint::slint! {
+    import {Button, StandardButton, ListView} from "std-widgets.slint";
+    export component MainWindow inherits Window {
+        title: "Recipe Manager";
+        min-width: 400px;
+        min-height: 550px;
+        in property<[string]> recipes: [];
+        callback edit_recipe(int);
+        callback delete_recipe(int);
+        callback new_recipe();
+
+        GridLayout {
+            spacing: 30px;
+            padding: 30px;
+            Text {
+                    max-height: 20px;
+                    text: "Recipe Manager";
+                    font-size: 18px;
+                    font-weight: 600;
+                    horizontal-alignment: center;
+                    row: 0;
+            }
+             GridLayout {
+                row: 1;
+                spacing: 10px;
+                Text {
+                    max-height: 20px;
+                    text: "Recipes";
+                    font-size: 14px;
+                    horizontal-alignment: center;
+                    row: 0;
+                }
+                VerticalLayout {
+                    row: 1;
+                    ListView {
+                        for recipe[index] in recipes: GridLayout{
+                            padding-left: 10px;
+                            padding-right: 10px;
+                            spacing: 5px;
+                            Text{
+                                text: recipe;
+                                font-size: 16px;
+                                vertical-alignment: center;
+                                horizontal-alignment: center;
+                                col: 0;
+                                colspan: 2;
+                            }
+                            Button {
+                                col: 2;
+                                max-width: 50px;
+                                text: "Edit";
+                                clicked => {edit_recipe(index);}
+                            }
+                            Button {
+                                col: 3;
+                                max-width: 60px;
+                                text: "Delete";
+                                clicked => {delete_recipe(index);}
+                            }
+                        }
+                    }
+                    Button {
+                            text: "New Recipe ...";
+                            clicked => {new_recipe();}
+                    }
+                }
+            }
+
+        }
+    }
 }
 
 slint::slint! {
@@ -9,6 +135,9 @@ slint::slint! {
         min-width: 400px;
         min-height: 550px;
         in-out property<string> ingredients: "Ingredient 1\nIngredient 2";
+        in-out property<string> name: "Recipe name";
+        in-out property<string> desc: "Recipe description";
+        callback return_from_editor(string,string,string);
         GridLayout {
             spacing: 30px;
             padding: 30px;
@@ -24,7 +153,7 @@ slint::slint! {
                 spacing: 10px;
                 Text {
                     max-height: 20px;
-                    text: "Recipe title";
+                    text: "Recipe name";
                     font-size: 14px;
                     horizontal-alignment: center;
                     row: 0;
@@ -32,7 +161,7 @@ slint::slint! {
                 LineEdit {
                     max-height: 20px;
                     font-size: 14px;
-                    placeholder-text: "Recipe title";
+                    placeholder-text: name;
                     row: 1;
                 }
             }
@@ -49,7 +178,7 @@ slint::slint! {
                 LineEdit {
                     max-height: 20px;
                     font-size: 14px;
-                    placeholder-text: "Recipe description";
+                    placeholder-text: desc;
                     row: 1;
                 }
             }
@@ -69,10 +198,13 @@ slint::slint! {
                     row: 1;
                 }
             }
-            StandardButton { 
+            StandardButton {
                 row: 4;
-                kind: ok; 
+                kind: ok;
                 min-height: 40px;
+                clicked => {
+                    root.return_from_editor(name,desc,ingredients);
+                }
             }
         }
     }
